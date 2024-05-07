f7813f940d4da1f6f31b92d466bdea9a01eb7af3
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-07 15:33:53 +0200

    Feat → added and included footer / header files into index.php

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    
+
+    <div class="footer text-center text-light bg-primary py-5">
+        <h1>
+            FOOOTEEEEEEEEEEEEEEEEEER
+        </h1>
+    </div>
+
+    
+</footer>
---
+++ components/header.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="container">
+
+        <div class="title py-5">
+            <h1>
+                Newsletter
+            </h1>
+        </div>
+
+    </div>
+</header>
+
+<style>
+    /* Header */
+
+    header{
+        background-color: coral;
+        color: white;
+        margin-bottom: 100px;
+    }
+
+    .title{
+        text-align: center;
+        
+    }
+</style>
