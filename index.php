8a137d465f7dd633d257e3db127fad048e2cbd54
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-07 15:57:05 +0200

    Refaxtor → changed danger alert message

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ include_once __DIR__ . '/utilities.php';
                 ?>
                     <div class="text-center alert alert-danger" role="alert">
                         <h2>
-                            Qualcosa è andato storto.
+                            Qualcosa è andato storto, controlla la tua mail.
                         </h2>
                         <a href="http://localhost/php-iscrizione-newsletter/">Riprova</a>
                     </div>
