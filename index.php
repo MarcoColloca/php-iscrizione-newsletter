7b9cc260156a0b5759b7617365065bd65592a007
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-07 15:34:09 +0200

    Add → added some style to index.php

--- index.php
+++ index.php
@@ -25,56 +25,49 @@ if($mail === null){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <header>
-        <div class="container">
-            
-            <div class="title py-5">
-                <h1>
-                    Newsletter
-                </h1>
-            </div>
-            
-        </div>
-    </header>
+    <div id="App">
 
-    <main>
-        <section class="newsletter">
-            <div class="container">
-                <div class="form-container">
-                    <form action="" method="POST">
-                        <label for="newsletter"> Inserisci la tua Mail:</label>
-                        <input type="email" name="newsletter" id="newsletter" placeholder="example@mail.com">
-
-                        <div class="button-container my-3">
-                            <button class="btn btn-primary"> Invia </button>
-                        </div>
-                    </form>
+        <?php include __DIR__ . '/components/header.php'; ?> 
+    
+        <main>
+            <section class="newsletter">
+                <div class="container">
+                    <div class="form-container">
+                        <form action="" method="POST">
+                            <label for="newsletter"> Inserisci la tua Mail:</label>
+                            <input type="email" name="newsletter" id="newsletter" placeholder="example@mail.com">
+    
+                            <div class="button-container my-3">
+                                <button class="btn btn-primary"> Invia </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
-
-       <?php include_once __DIR__ . '/utilities.php'?>
-    </main>
+            </section>
+    
+           <?php include_once __DIR__ . '/utilities.php'?>
+        </main>
+    
+    
+        <?php include __DIR__ . '/components/footer.php'; ?>
+    </div>
 </body>
 
 
 <style>
 
-    /* Header */
-
-    header{
-        background-color: coral;
-        color: white;
-        margin-bottom: 100px;
-    }
-
-    .title{
-        text-align: center;
-        
+    #App{
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
     }
 
 
     /* Main */
+
+    main{
+       flex-grow: 1;
+    }
 
     .newsletter label{
         display: block;
@@ -89,6 +82,6 @@ if($mail === null){
         display: flex;
         justify-content: center;
     }
-    
+
 </style>
 </html>
